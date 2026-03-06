99e4353cf12de2bd3bcd3bd8a19a69ecf6a32a7a
Andrey9045 <andreidikun123@gmail.com>
2026-03-06 14:57:21 +0400

    Добавил debugtoolbar

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from places import views
 from django.conf import settings
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls 
 
 
 urlpatterns = [
@@ -32,3 +33,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += debug_toolbar_urls()
